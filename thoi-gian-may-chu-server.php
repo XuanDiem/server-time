ead9e61edfcc351c7a87f3e1705512d280533be4
VU XUAN DIEM <48935047+XuanDiem@users.noreply.github.com>
2019-07-26 02:53:40 +0700

    Add files via upload

---
+++ thoi-gian-may-chu-server.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Thoi gian hien tai</title>
+</head>
+<body>
+<h1> Bay gio la:<?php echo date('Y-M-d h:m:s');?></h1>
+
+</body>
+</html>
